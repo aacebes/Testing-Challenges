47f3ab786154964ee04dbe43dd650c93ac8a3af9
Alejandro Acebes Cabrera <aacebes@autentia.com>
2016-05-30 20:41:15 +0200

    Test for the input data from a file.

--- src/test/TeamLunchTest.java
+++ src/test/TeamLunchTest.java
@@ -6,20 +6,34 @@ import org.junit.Test;
 
 import main.TeamLunch;
 
-
 public class TeamLunchTest {
+
     TeamLunch teamLunch = new TeamLunch();
 
     @Test
-    public void inputTest() {
-        fail("Not yet implemented");
+    public void shouldCorrectlyInitializeDataStructureFromAnInputFile() throws FileNotFoundException {
+        // Given
+        String inputFileRoute = "files/tests/test_input.txt";
+
+        // When
+        teamLunch.initializeDinnersDataFromInputFile(inputFileRoute);
+
+        // Then
+        int casesAmount = teamLunch.getCasesAmount();
+        assertThat(casesAmount, is(3));
+
+        int[] dinnersToSitForEachCase = teamLunch.getDinnnersForEachCase();
+        assertThat(dinnersToSitForEachCase.length, is(casesAmount));
+        assertThat(dinnersToSitForEachCase[0], is(24));
+        assertThat(dinnersToSitForEachCase[1], is(5913));
+        assertThat(dinnersToSitForEachCase[2], is(3));
     }
 
     @Test
     public void outputTest() {
         fail("Not yet implemented");
     }
-    
+
     @Test
     public void algorithmTest() {
         fail("Not yet implemented");
