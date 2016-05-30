f096a366c6eeecdf62fe9db4ff2e074aef2825e8
Alejandro Acebes Cabrera <aacebes@autentia.com>
2016-05-30 20:33:37 +0200

    Structure of the project for a challenge.

---
+++ src/test/TeamLunchTest.java
@@ -0,0 +1,27 @@
+package test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import main.TeamLunch;
+
+
+public class TeamLunchTest {
+    TeamLunch teamLunch = new TeamLunch();
+
+    @Test
+    public void inputTest() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void outputTest() {
+        fail("Not yet implemented");
+    }
+    
+    @Test
+    public void algorithmTest() {
+        fail("Not yet implemented");
+    }
+}
